d3b916a337cc85d09c1de121f4dbc7a33eef7347
Johnny Ferreira da Silva <joferres@everis.com>
2021-07-20 00:23:28 -0300

    estilos globais do app

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
 
 import Head from 'next/head';
 
-import styles from '../styles/home.module.scss';
 
 export default function Home() {
   return (
@@ -9,7 +8,7 @@ export default function Home() {
       <Head>
         <title>Início | ig.news</title>
       </Head>
-     <h1 className={styles.title}>Hello <span>World</span></h1>
+     <h1>Hello World</h1>
    </>
   )
 }
