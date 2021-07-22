d034056db69cbb8b16a2ff5ec82ce696073fc3ba
Johnny Ferreira da Silva <joferres@everis.com>
2021-07-21 23:39:18 -0300

    Autenticação com Next Auth

--- src/components/Header/SignInButton/index.tsx
+++ src/components/Header/SignInButton/index.tsx
@@ -1,19 +1,21 @@
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi';
+import { signIn, signOut, useSession } from 'next-auth/client';
 
 import styles from './styles.module.scss';
 
 export function SignInButton() {
 
-    const isUserLoggedIn = true;
+    const [session] = useSession(); 
 
-    return isUserLoggedIn ? (
+    return session ? (
         <button 
             type="button"
             className={styles.signInButton}
+            onClick={() => signOut()}
             >
         <FaGithub  color="#04d361"/>
-            Johnny Silva
+            {session.user.name}
             <FiX 
                 color="#737380"
                 className={styles.closeIcon}
@@ -24,6 +26,7 @@ export function SignInButton() {
         <button 
             type="button"
             className={styles.signInButton}
+            onClick={() => signIn('github')}
             >
         <FaGithub  color="#eba417"/>
             Sign in with Github
