feca295e1489be5a2668ded8e2955ae4d070736d
Johnny Ferreira da Silva <joferres@everis.com>
2021-07-21 21:58:38 -0300

    Consumindo API do Stripe SSR

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,18 @@
 
 import Head from 'next/head';
+import { GetServerSideProps } from 'next/'
 import { SubscribeButton } from '../components/SubscribeButton';
 import styles from './home.module.scss'
+import {stripe } from '../services/stripe';
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  }
+}
+
+export default function Home({ product }: HomeProps) {
   return (
    <>
       <Head>
@@ -16,13 +25,30 @@ export default function Home() {
           <h1>News about the <span>React</span> world.</h1>
           <p>
             Get access to all the publication <br />
-            <span>for $9.90 month</span>
+            <span>for {product.amount} month</span>
           </p>
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </section>
 
         <img src="/images/avatar.svg" alt="Girl coding" />
       </main>
    </>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const price = await stripe.prices.retrieve('price_1JFU1XGcU6w6hwcMoY3fBYg0');
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD'
+    }).format(price.unit_amount / 100, )
+  }
+  return {
+    props: {
+      product
+    }
+  }
 }
