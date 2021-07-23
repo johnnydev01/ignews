3793feb33c97d84521e9c475fe42f7f47b71ad04
Johnny Ferreira da Silva <joferres@everis.com>
2021-07-22 22:14:28 -0300

    salvando usuário no banco

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
+import { query as q } from 'faunadb';
+
 import NextAuth from 'next-auth'
 import Providers from 'next-auth/providers'
+
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -11,5 +15,25 @@ export default NextAuth({
     }),
     // ...add more providers here
   ],
+  jwt: {
+    signingKey: process.env.SIGNING_KEY
+  },
 
+  callbacks: {
+    async signIn(user, account, profile){
+      const { email } = user;
+     try {
+        await fauna.query(
+          q.Create(
+            q.Collection('users'),
+            {data: {email}}
+          )
+        )
+        return true;
+     } catch (error) {
+       
+       return false;
+     }
+    }
+  }
 })
